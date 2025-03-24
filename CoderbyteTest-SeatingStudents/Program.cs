bed60416747c0b27794581316c18efe747fd9d9e
aleksandr.chukanov <aleksandr.chukanov@krit.pro>
2025-03-25 00:36:58 +0300

    1st commit fixed

--- CoderbyteTest-SeatingStudents/Program.cs
+++ CoderbyteTest-SeatingStudents/Program.cs
@@ -7,32 +7,42 @@ using System.Threading.Tasks;
 namespace CoderbyteTest_SeatingStudents
 {
     internal class Program
-    {
-        //arr = [12, 2, 6, 7, 11]  return 6,   [2,1] => 0
+    {       
         static int MainProgram(int [] arr)
         {
             int k = arr[0]; //K seats
             int occupiedSeats = arr.Length - 1;
 
-            int totalPairs = 3 * k /2 - 2;
+            int totalPairs = 3 * k /2 - 2;                    
 
-            int occupiedPairs = 0;
+            List<string> list = new List<string>();
 
-            for (int i = 1; i <= occupiedSeats; i++)
+            int p = 1;
+            int n = 1;
+            int m = 1;
+
+            for (int i = 0; i < k/2; i++)
             {
-                if (arr[i] == 1 || arr[i] == 2 || arr[i] == k || arr[i] == k-1)
-                {
-                    occupiedPairs += 2;
-                }
-                else 
-                {
-                    occupiedPairs += 3;
+                list.Add($"{(n++).ToString("00")},{n++.ToString("00")}");
+
+                if (i < k / 2 - 1)
+                {                   
+                    list.Add($"{(m++).ToString("00")},{(m++ + 1).ToString("00")}");                   
+                    list.Add($"{(p++ + 1).ToString("00")},{(p++ + 2).ToString("00")}");
                 }
             }
 
-            int seatStudents = totalPairs - occupiedPairs;
+            if (totalPairs != list.Count)
+            {
+                Console.WriteLine($" Error: totalPairs != list.Count, {totalPairs} != {list.Count}");
+            }
 
-            return seatStudents;
+            for (int i = 1; i <= occupiedSeats; i++)
+            {                
+                list.RemoveAll(x => x.Contains(arr[i].ToString("00")));
+            }
+
+            return list.Count();
         }
         static void Main(string[] args)
         {
@@ -40,10 +50,25 @@ namespace CoderbyteTest_SeatingStudents
             int[] arr2 = { 6, 4 };
             int[] arr3 = { 8, 1, 8 };
 
+            //int[] arr4 = { 12, 1, 2, 6, 7, 11, 12 };
+            //int[] arr5 = { 2, 1 };
+            //int[] arr6 = { 4, 2 };
+            //int[] arr6 = {4, 4};
+            //int[] arr7 = { 4, 1, 4 };
+            //int[] arr7 = { 4, 1, 3 };
+            //int[] arr8 = { 12, 11, 12 };
+
             Console.WriteLine( $" Input: [12, 2, 6, 7, 11], Output: {MainProgram(arr1)}"); //6
             Console.WriteLine($" Input: [6, 4], Output: {MainProgram(arr2)}");             //4
             Console.WriteLine($" Input: [8, 1, 8], Output: {MainProgram(arr3)}");          //6
-
+            //Console.WriteLine($" Input: [12, 1, 2, 6, 7, 11, 12], Output: {MainProgram(arr4)}"); //4
+            //Console.WriteLine($" Input: [2, 2], Output: {MainProgram(arr5)}"); //0
+            //Console.WriteLine($" Input: [4,2], Output: {MainProgram(arr6)}"); //2
+            //Console.WriteLine($" Input: [4,4], Output: {MainProgram(arr6)}"); //2
+            //Console.WriteLine($" Input: [4,1,4], Output: {MainProgram(arr7)}"); //0
+            //Console.WriteLine($" Input: [4,1,3], Output: {MainProgram(arr7)}"); //1
+            //Console.WriteLine($" Input: [12, 11], Output: {MainProgram(arr8)}"); //14
+           // Console.WriteLine($" Input: [12, 11, 12], Output: {MainProgram(arr8)}"); //13
         }
     }
 }
